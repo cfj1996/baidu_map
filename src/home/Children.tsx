952bab8dc1cb3560b847e0194d9127231d3332d1
陈方杰 <iloveweb1996@gmail.com>
2020-06-25 05:04:21 +0800

    feat: 三级联动

--- src/home/Children.tsx
+++ src/home/Children.tsx
@@ -1,10 +1,12 @@
 import React, { FC, useState, useEffect, useMemo } from "react";
 import { Box, List, ListItem, ListItemText } from "@material-ui/core";
+import Divider from "@material-ui/core/Divider";
 export interface City {
   value: number | string;
   label: string;
   children?: City[];
 }
+
 const children: FC<{
   data: City[];
   onPass: (data: City, index: number) => void;
@@ -12,26 +14,28 @@ const children: FC<{
   selectedValue?: number | string;
 }> = ({ data, onPass, index, selectedValue }) => {
   return (
-    <List
-      style={{
-        height: "400px",
-        width: "140px",
-        display: "inline-block",
-        overflow: "auto",
-        borderRight: "1px #330 solid",
-      }}
-    >
-      {data.map((i) => (
-        <ListItem
-          selected={i.value === selectedValue}
-          button
-          key={i.value}
-          onClick={() => onPass(i, index)}
-        >
-          <ListItemText primary={i.label} />
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <List
+        style={{
+          height: "400px",
+          width: "140px",
+          display: "inline-block",
+          overflow: "auto",
+        }}
+      >
+        {data.map((i) => (
+          <ListItem
+            selected={i.value === selectedValue}
+            button
+            key={i.value}
+            onClick={() => onPass(i, index)}
+          >
+            <ListItemText primary={i.label} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider style={{ height: "400px" }} orientation="vertical" flexItem />
+    </>
   );
 };
 
