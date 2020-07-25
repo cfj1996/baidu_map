ea67156790406adcf9fe00fdc219e76084d656a2
陈方杰 <iloveweb1996@gmail.com>
2020-07-26 01:52:05 +0800

    feat: 请求地址

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,27 +1,39 @@
 import * as d3 from "d3";
-import { schemeSet1 } from "d3-scale-chromatic";
 import React, { FC, useEffect } from "react";
-
-import { scaleOrdinal } from "@vx/scale";
+const colorScale = d3.scaleOrdinal(d3.schemeAccent);
 
 const parent = [];
-const svgWidth = 1200;
-const svgHeight = 900;
-const padding = 60;
-const colorScale = scaleOrdinal({
-  range: schemeSet1,
-});
+const svgWidth = 1000;
+const svgHeight = 1000;
+const padding = 40;
+/**
+ * 地理坐标转换为桌面坐标
+ * @param geoJson
+ */
+const geoToScreenCoordinate = (geoJson) => {
+  const x0 = padding;
+  const y0 = padding;
+  const x1 = svgWidth - padding * 2;
+  const y1 = svgHeight - padding * 2;
+  return d3.geoMercator().fitExtent(
+    [
+      [x0, y0],
+      [x1, y1],
+    ],
+    geoJson,
+  );
+};
 
+/**
+ * 请求地理坐标数据
+ * @param code
+ */
 const getData = async (code) => {
   const response = await fetch("/api/assets/map/full/" + code + "_full.json");
   return await response.json();
 };
 
-const render = (geoJson) => {
-  const x0 = padding;
-  const y0 = padding;
-  const x1 = svgWidth - padding * 2;
-  const y1 = svgHeight - padding * 2;
+const renderMap = (geoJson) => {
   const svg = d3
     .select(".chart")
     .attr("height", svgHeight)
@@ -32,57 +44,82 @@ const render = (geoJson) => {
         const code = parent.pop();
         if (code) {
           const geoJson = await getData(code);
-          svg.selectAll("path").remove();
-          render(geoJson);
+          svg.selectAll("*").remove();
+          renderMap(geoJson);
         }
       }
-    })
+    });
+
+  const pathGenerator = d3.geoPath().projection(geoToScreenCoordinate(geoJson));
+  const G = svg
+    .selectAll("path")
+    .data(geoJson.features) //数据绑定
+    .enter()
     .append("g")
     .attr("fill", "none")
     .attr("stroke", "black")
-    .attr("stroke-width", "1");
-  const pathGenerator = d3.geoPath().projection(
-    d3.geoMercator().fitExtent(
-      [
-        [x0, y0],
-        [x1, y1],
-      ],
-      geoJson,
-    ),
-  );
-  const mapPath = svg
-    .selectAll("path")
-    .data(geoJson.features) //数据绑定
-    .join("path")
-    .attr("d", pathGenerator) //绘制path
-    .attr("stroke-dasharray", "2,5")
-    .on("click", async function (data) {
+    .attr("stroke-width", "1")
+    .on("click", async function (data, mg) {
+      const e = d3.event;
       getData(data.properties.adcode)
         .then((geoJson) => {
           if (geoJson.type) {
             parent.push(data.properties.parent.adcode);
-            svg.selectAll("path").remove();
-            render(geoJson);
+            svg.selectAll("*").remove();
+            renderMap(geoJson);
           }
         })
         .catch((err) => {
           console.log(err);
         });
-    })
+      e.stopPropagation();
+    });
+  G.append("path")
+    .attr("d", pathGenerator) //绘制path
+    .attr("stroke-dasharray", "2,5")
     .attr("fill", (d, i) => {
       return colorScale(i);
     });
-  return mapPath;
+  renderText(G, geoJson);
+};
+
+const renderText = (G, geoJson) => {
+  G.append("text")
+    .attr("x", function (d, i) {
+      const projection = geoToScreenCoordinate(geoJson);
+      const data = d.properties.centroid || d.properties.center;
+      if (data) {
+        return projection(data)[0] - 15;
+      }
+    })
+    .attr("y", function (d, i) {
+      const projection = geoToScreenCoordinate(geoJson);
+      const data = d.properties.centroid || d.properties.center;
+      if (data) {
+        return projection(data)[1] - 5;
+      }
+    })
+    .text(function (d, i) {
+      return d.properties.name;
+    })
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "12px")
+    .style("z-index", "9999");
 };
 
 const AboutPage: FC = function (props) {
   useEffect(() => {
     getData(100000).then((res) => {
-      render(res);
+      renderMap(res);
     });
   }, []);
 
-  return <svg className="chart" />;
+  return (
+    <div>
+      <svg className="chart" />
+      <div className="dom" />
+    </div>
+  );
 };
 
 export default AboutPage;
